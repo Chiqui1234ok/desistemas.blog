47d38c531c29d4fc83e91b618a1cabe50aeb6984
Santiago Gimenez <santiagogimenez@outlook.com.ar>
2025-10-11 16:27:30 +0000

    hot fix part 1

--- _plugins/lazy_images.rb
+++
@@ -1,7 +0,0 @@
-Jekyll::Hooks.register :documents, :post_write do |doc|
-  if doc.output_ext == '.html'
-    content = File.read(doc.destination(doc.site.dest))
-    content.gsub!(/<img(?![^>]*loading=)/, '<img loading="lazy" decoding="async"')
-    File.write(doc.destination(doc.site.dest), content)
-  end
-end
